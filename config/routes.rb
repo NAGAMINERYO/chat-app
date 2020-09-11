cfc9b3099a10783cf0b6012ab53ab6b5d71c59f0
NAGAMINERYO <70889037+NAGAMINERYO@users.noreply.github.com>
2020-09-11 16:10:19 +0900

    revert revert ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   get 'messages/index'
+=======
+  devise_for :users
+>>>>>>> parent of 29cca22... Revert "ユーザー編集機能の実装"
   root to: "messages#index"
+  resources :users, only: [:edit, :update]
 end
