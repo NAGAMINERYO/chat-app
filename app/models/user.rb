475a662b78a94411944c6b28412553b33728ec9a
NAGAMINERYO <70889037+NAGAMINERYO@users.noreply.github.com>
2020-09-11 16:14:29 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-<<<<<<< HEAD
+
 
 validates :name, presence: true
 
-=======
->>>>>>> parent of 843fc05... Revert "usersテーブルの作成"
+
 end
